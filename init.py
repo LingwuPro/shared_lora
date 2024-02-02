ae271d359dbeef5c47929864613dd59cb6f65ea1
LinGW-u64 <92579838+LingwuPro@users.noreply.github.com>
2024-02-02 17:56:12 +0800

    Update init.py

--- init.py
+++ init.py
@@ -1,6 +1,7 @@
 from transformers import LlamaForCausalLM, LlamaTokenizer, AutoTokenizer
 from loraLlama import LlamaForCausalLM as redoLlamaForCausalLM
 from datasets import Dataset
+import torch.nn as nn
 from transformers import DataCollator
 from configuration_llama import LlamaConfig
 from torch.nn import Linear
@@ -73,14 +74,22 @@ class initialize:
             self.attn_list = get_attn_dict(attn_list, self.lora_groupby, self.q_layer_dict, self.k_layer_dict, self.v_layer_dict, self.o_layer_dict)
             config.attn_list = self.attn_list
         else:
-            group_size = self.lora_groupby // self.num_layers
+            group_size = self.num_layers // self.lora_groupby
             for idx in range(self.lora_groupby):
                 temp_list = [str(i) for i in range(idx * group_size, (idx + 1) * group_size)]
                 self.attn_list.append(','.join(temp_list))
             config.attn_list = self.attn_list
+            for group in range(self.lora_groupby):
+                for layer in range(group_size):
+                    real_layer = group * group_size + layer
+                    self.lora_model.model.layers[group].sub_layers[layer].self_attn.rotary_emb = self.model.model.layers[real_layer].self_attn.rotary_emb
+                    self.lora_model.model.layers[group].sub_layers[layer].input_layernorm = self.model.model.layers[real_layer].input_layernorm
+                    self.lora_model.model.layers[group].sub_layers[layer].post_attention_layernorm = self.model.model.layers[real_layer].post_attention_layernorm
+                    self.lora_model.model.layers[group].sub_layers[layer].mlp.act_fn = self.model.model.layers[real_layer].mlp.act_fn
+                    
         
         if config.use_ffn_match == True:
-            self.attn_list = reshape_ffn_weight(self.attn_list, self.gate_layer_dict, self.up_layer_dict, self.down_layer_dict)
+            self.gate_layer_dict, self.up_layer_dict, self.down_layer_dict = reshape_ffn_weight(self.attn_list, self.gate_layer_dict, self.up_layer_dict, self.down_layer_dict)
             
  
     def avg_W(self) -> None:
@@ -95,6 +104,14 @@ class initialize:
             up_num_rows, up_num_cols = next(iter(self.up_layer_dict.values())).shape
             down_num_rows, down_num_cols = next(iter(self.down_layer_dict.values())).shape
             
+            q_weight = []
+            k_weight = []
+            v_weight = []
+            o_weight = []
+            gate_weight = []
+            up_weight = []
+            down_weight = []
+
             for layer_index in range(groupby_layer_size):
                 if self.use_attn_match == True:
                     real_layer = self.attn_list[groupby_index][layer_index]
@@ -102,56 +119,48 @@ class initialize:
                     real_layer = groupby_index * groupby_layer_size + layer_index
                 
                 #W_q
-                q_weight = []
                 q_weight.append(self.q_layer_dict["model.layers.{real_layer}".format(real_layer = real_layer)].view(-1))
                 #W_k
-                k_weight = []
                 k_weight.append(self.k_layer_dict["model.layers.{real_layer}".format(real_layer = real_layer)].view(-1))
                 #W_v
-                v_weight = []
                 v_weight.append(self.v_layer_dict["model.layers.{real_layer}".format(real_layer = real_layer)].view(-1))
                 #W_o
-                o_weight = []
                 o_weight.append(self.o_layer_dict["model.layers.{real_layer}".format(real_layer = real_layer)].view(-1))
                 #W_gate
-                gate_weight = []
                 gate_weight.append(self.gate_layer_dict["model.layers.{real_layer}".format(real_layer = real_layer)].view(-1))
                 #W_up
-                up_weight = []
                 up_weight.append(self.up_layer_dict["model.layers.{real_layer}".format(real_layer = real_layer)].view(-1))
                 #W_down
-                down_weight = []
                 down_weight.append(self.down_layer_dict["model.layers.{real_layer}".format(real_layer = real_layer)].view(-1))
                 
-                if self.use_attn_match == True:
-                    self.lora_model.model.groupby_index.W_q.weight.copy_(torch.mean(torch.stack(q_weight), dim=0).view(q_num_rows, q_num_cols))
-                    self.lora_model.model.groupby_index.W_q.weight.copy_(torch.mean(torch.stack(q_weight), dim=0).view(q_num_rows, q_num_cols))
-                    self.lora_model.model.groupby_index.W_k.weight.copy_(torch.mean(torch.stack(k_weight), dim=0).view(k_num_rows, k_num_cols))
-                    self.lora_model.model.groupby_index.W_v.weight.copy_(torch.mean(torch.stack(v_weight), dim=0).view(v_num_rows, v_num_cols))
-                    self.lora_model.model.groupby_index.W_o.weight.copy_(torch.mean(torch.stack(o_weight), dim=0).view(o_num_rows, o_num_cols))
-                    self.lora_model.model.groupby_index.W_gate.weight.copy_(torch.mean(torch.stack(gate_weight), dim=0).view(gate_num_rows, gate_num_cols))
-                    self.lora_model.model.groupby_index.W_up.weight.copy_(torch.mean(torch.stack(up_weight), dim=0).view(up_num_rows, up_num_cols))
-                    self.lora_model.model.groupby_index.W_down.weight.copy_(torch.mean(torch.stack(down_weight), dim=0).view(down_num_rows, down_num_cols))
-                else:
-                    self.lora_model.model.layers[groupby_index].W_q.weight.detach().copy_(
-                        torch.mean(torch.stack(q_weight), dim=0).view(q_num_rows, q_num_cols))
-                    self.lora_model.model.layers[groupby_index].W_k.weight.detach().copy_(
-                        torch.mean(torch.stack(k_weight), dim=0).view(k_num_rows, k_num_cols))
-                    self.lora_model.model.layers[groupby_index].W_v.weight.detach().copy_(
-                        torch.mean(torch.stack(v_weight), dim=0).view(v_num_rows, v_num_cols))
-                    self.lora_model.model.layers[groupby_index].W_o.weight.detach().copy_(
-                        torch.mean(torch.stack(o_weight), dim=0).view(o_num_rows, o_num_cols))
-                    self.lora_model.model.layers[groupby_index].W_gate.weight.detach().copy_(
-                        torch.mean(torch.stack(gate_weight), dim=0).view(gate_num_rows, gate_num_cols))
-                    self.lora_model.model.layers[groupby_index].W_up.weight.detach().copy_(
-                        torch.mean(torch.stack(up_weight), dim=0).view(up_num_rows, up_num_cols))
-                    self.lora_model.model.layers[groupby_index].W_down.weight.detach().copy_(
-                        torch.mean(torch.stack(down_weight), dim=0).view(down_num_rows, down_num_cols))
+            if self.use_attn_match == True:
+                self.lora_model.model.groupby_index.W_q.weight.copy_(torch.mean(torch.stack(q_weight), dim=0).view(q_num_rows, q_num_cols))
+                self.lora_model.model.groupby_index.W_k.weight.copy_(torch.mean(torch.stack(k_weight), dim=0).view(k_num_rows, k_num_cols))
+                self.lora_model.model.groupby_index.W_v.weight.copy_(torch.mean(torch.stack(v_weight), dim=0).view(v_num_rows, v_num_cols))
+                self.lora_model.model.groupby_index.W_o.weight.copy_(torch.mean(torch.stack(o_weight), dim=0).view(o_num_rows, o_num_cols))
+                self.lora_model.model.groupby_index.W_gate.weight.copy_(torch.mean(torch.stack(gate_weight), dim=0).view(gate_num_rows, gate_num_cols))
+                self.lora_model.model.groupby_index.W_up.weight.copy_(torch.mean(torch.stack(up_weight), dim=0).view(up_num_rows, up_num_cols))
+                self.lora_model.model.groupby_index.W_down.weight.copy_(torch.mean(torch.stack(down_weight), dim=0).view(down_num_rows, down_num_cols))
+            else:
+                self.lora_model.model.layers[groupby_index].W_q.weight = nn.Parameter(
+                    torch.mean(torch.stack(q_weight), dim=0).view(q_num_rows, q_num_cols))
+                self.lora_model.model.layers[groupby_index].W_k.weight = nn.Parameter(
+                    torch.mean(torch.stack(k_weight), dim=0).view(k_num_rows, k_num_cols))
+                self.lora_model.model.layers[groupby_index].W_v.weight = nn.Parameter(
+                    torch.mean(torch.stack(v_weight), dim=0).view(v_num_rows, v_num_cols))
+                self.lora_model.model.layers[groupby_index].W_o.weight = nn.Parameter(
+                    torch.mean(torch.stack(o_weight), dim=0).view(o_num_rows, o_num_cols))
+                self.lora_model.model.layers[groupby_index].W_gate.weight = nn.Parameter(
+                    torch.mean(torch.stack(gate_weight), dim=0).view(gate_num_rows, gate_num_cols))
+                self.lora_model.model.layers[groupby_index].W_up.weight = nn.Parameter(
+                    torch.mean(torch.stack(up_weight), dim=0).view(up_num_rows, up_num_cols))
+                self.lora_model.model.layers[groupby_index].W_down.weight = nn.Parameter(
+                    torch.mean(torch.stack(down_weight), dim=0).view(down_num_rows, down_num_cols))
 
     def get_diff_svd(self) -> None:
         groupby_layer_size = self.num_layers // self.lora_groupby
         for groupby_index in tqdm(range(self.lora_groupby), desc="svd_groupby"):
-            for layer_index in tqdm(range(groupby_layer_size), desc="layer"):
+            for layer_index in tqdm(range(groupby_layer_size), desc=f"layer {groupby_index}"):
                 if self.use_attn_match == True:
                     real_layer = self.attn_list[groupby_index][layer_index]
                 else:
@@ -163,24 +172,24 @@ class initialize:
                     U = U[:,:self.lora_size]
                     sigma = torch.diag(sigma[:self.lora_size])
                     V = V[:,:self.lora_size].T
-                    self.lora_model.model.layers.real_layer.self_attn.A_q.weight.copy_(U @ torch.sqrt(sigma))
-                    self.lora_model.model.layers.real_layer.self_attn.B_q.weight.copy_(torch.sqrt(sigma) @ V)
+                    self.lora_model.model.layers.real_layer.self_attn.A_q.weight = nn.Parameter(U @ torch.sqrt(sigma))
+                    self.lora_model.model.layers.real_layer.self_attn.B_q.weight = nn.Parameter(torch.sqrt(sigma) @ V)
                     # W_k
                     k_diff = self.k_layer_dict["model.layers.{real_layer}".format(real_layer = real_layer)] - self.lora_model.model.groupby_index.W_k.weight
                     U, sigma, V = torch.svd(k_diff)
                     U = U[:,:self.lora_size]
                     sigma = torch.diag(sigma[:self.lora_size])
                     V = V[:,:self.lora_size].T
-                    self.lora_model.model.layers.real_layer.self_attn.A_k.weight.copy_(U @ torch.sqrt(sigma))
-                    self.lora_model.model.layers.real_layer.self_attn.B_k.weight.copy_(torch.sqrt(sigma) @ V)
+                    self.lora_model.model.layers.real_layer.self_attn.A_k.weight = nn.Parameter(U @ torch.sqrt(sigma))
+                    self.lora_model.model.layers.real_layer.self_attn.B_k.weight = nn.Parameter(torch.sqrt(sigma) @ V)
                     # W_v
                     v_diff = self.v_layer_dict["model.layers.{real_layer}".format(real_layer = real_layer)] - self.lora_model.model.groupby_index.W_v.weight
                     U, sigma, V = torch.svd(v_diff)
                     U = U[:,:self.lora_size]
                     sigma = torch.diag(sigma[:self.lora_size])
                     V = V[:,:self.lora_size].T
-                    self.lora_model.model.layers.real_layer.self_attn.A_v.weight.copy_(U @ torch.sqrt(sigma))
-                    self.lora_model.model.layers.real_layer.self_attn.B_v.weight.copy_(torch.sqrt(sigma) @ V)
+                    self.lora_model.model.layers.real_layer.self_attn.A_v.weight = nn.Parameter(U @ torch.sqrt(sigma))
+                    self.lora_model.model.layers.real_layer.self_attn.B_v.weight = nn.Parameter(torch.sqrt(sigma) @ V)
                     # W_o
                     o_diff = self.o_layer_dict["model.layers.{real_layer}".format(real_layer = real_layer)] - self.lora_model.model.groupby_index.W_o.weight
                     U, sigma, V = torch.svd(o_diff)
@@ -220,58 +229,59 @@ class initialize:
                     U = U[:,:self.lora_size]
                     sigma = torch.diag(sigma[:self.lora_size])
                     V = V[:,:self.lora_size].T
+                    print(q_diff)
                     # temp = U @ torch.sqrt(sigma)
                     # print(temp.shape, self.lora_model.model.layers[groupby_index].sub_layers[layer_index].self_attn.A_q.weight.shape)
-                    self.lora_model.model.layers[groupby_index].sub_layers[layer_index].self_attn.A_q.weight.detach().copy_(U @ torch.sqrt(sigma))
-                    self.lora_model.model.layers[groupby_index].sub_layers[layer_index].self_attn.B_q.weight.detach().copy_(torch.sqrt(sigma) @ V)
+                    self.lora_model.model.layers[groupby_index].sub_layers[layer_index].self_attn.A_q.weight = nn.Parameter(U @ torch.sqrt(sigma))
+                    self.lora_model.model.layers[groupby_index].sub_layers[layer_index].self_attn.B_q.weight = nn.Parameter(torch.sqrt(sigma) @ V)
                     # W_k
                     k_diff = self.k_layer_dict["model.layers.{real_layer}".format(real_layer = real_layer)] - self.lora_model.model.layers[groupby_index].W_k.weight
                     U, sigma, V = torch.svd(k_diff)
                     U = U[:,:self.lora_size]
                     sigma = torch.diag(sigma[:self.lora_size])
                     V = V[:,:self.lora_size].T
-                    self.lora_model.model.layers[groupby_index].sub_layers[layer_index].self_attn.A_k.weight.detach().copy_(U @ torch.sqrt(sigma))
-                    self.lora_model.model.layers[groupby_index].sub_layers[layer_index].self_attn.B_k.weight.detach().copy_(torch.sqrt(sigma) @ V)
+                    self.lora_model.model.layers[groupby_index].sub_layers[layer_index].self_attn.A_k.weight = nn.Parameter(U @ torch.sqrt(sigma))
+                    self.lora_model.model.layers[groupby_index].sub_layers[layer_index].self_attn.B_k.weight = nn.Parameter(torch.sqrt(sigma) @ V)
                     # W_v
                     v_diff = self.v_layer_dict["model.layers.{real_layer}".format(real_layer = real_layer)] - self.lora_model.model.layers[groupby_index].W_v.weight
                     U, sigma, V = torch.svd(v_diff)
                     U = U[:,:self.lora_size]
                     sigma = torch.diag(sigma[:self.lora_size])
                     V = V[:,:self.lora_size].T
-                    self.lora_model.model.layers[groupby_index].sub_layers[layer_index].self_attn.A_v.weight.detach().copy_(U @ torch.sqrt(sigma))
-                    self.lora_model.model.layers[groupby_index].sub_layers[layer_index].self_attn.B_v.weight.detach().copy_(torch.sqrt(sigma) @ V)
+                    self.lora_model.model.layers[groupby_index].sub_layers[layer_index].self_attn.A_v.weight = nn.Parameter(U @ torch.sqrt(sigma))
+                    self.lora_model.model.layers[groupby_index].sub_layers[layer_index].self_attn.B_v.weight = nn.Parameter(torch.sqrt(sigma) @ V)
                     # W_o
                     o_diff = self.o_layer_dict["model.layers.{real_layer}".format(real_layer = real_layer)] - self.lora_model.model.layers[groupby_index].W_o.weight
                     U, sigma, V = torch.svd(o_diff)
                     U = U[:,:self.lora_size]
                     sigma = torch.diag(sigma[:self.lora_size])
                     V = V[:,:self.lora_size].T
-                    self.lora_model.model.layers[groupby_index].sub_layers[layer_index].self_attn.A_o.weight.detach().copy_(U @ torch.sqrt(sigma))
-                    self.lora_model.model.layers[groupby_index].sub_layers[layer_index].self_attn.B_o.weight.detach().copy_(torch.sqrt(sigma) @ V)
+                    self.lora_model.model.layers[groupby_index].sub_layers[layer_index].self_attn.A_o.weight = nn.Parameter(U @ torch.sqrt(sigma))
+                    self.lora_model.model.layers[groupby_index].sub_layers[layer_index].self_attn.B_o.weight = nn.Parameter(torch.sqrt(sigma) @ V)
                     # W_gate
                     gate_diff = self.gate_layer_dict["model.layers.{real_layer}".format(real_layer = real_layer)] - self.lora_model.model.layers[groupby_index].W_gate.weight
                     U, sigma, V = torch.svd(gate_diff)
                     U = U[:,:self.lora_size]
                     sigma = torch.diag(sigma[:self.lora_size])
                     V = V[:,:self.lora_size].T
-                    self.lora_model.model.layers[groupby_index].sub_layers[layer_index].mlp.A_gate.weight.detach().copy_(U @ torch.sqrt(sigma))
-                    self.lora_model.model.layers[groupby_index].sub_layers[layer_index].mlp.B_gate.weight.detach().copy_(torch.sqrt(sigma) @ V)
+                    self.lora_model.model.layers[groupby_index].sub_layers[layer_index].mlp.A_gate.weight = nn.Parameter(U @ torch.sqrt(sigma))
+                    self.lora_model.model.layers[groupby_index].sub_layers[layer_index].mlp.B_gate.weight = nn.Parameter(torch.sqrt(sigma) @ V)
                     # W_up
                     up_diff = self.up_layer_dict["model.layers.{real_layer}".format(real_layer = real_layer)] - self.lora_model.model.layers[groupby_index].W_up.weight
                     U, sigma, V = torch.svd(up_diff)
                     U = U[:,:self.lora_size]
                     sigma = torch.diag(sigma[:self.lora_size])
                     V = V[:,:self.lora_size].T
-                    self.lora_model.model.layers[groupby_index].sub_layers[layer_index].mlp.A_up.weight.detach().copy_(U @ torch.sqrt(sigma))
-                    self.lora_model.model.layers[groupby_index].sub_layers[layer_index].mlp.B_up.weight.detach().copy_(torch.sqrt(sigma) @ V)
+                    self.lora_model.model.layers[groupby_index].sub_layers[layer_index].mlp.A_up.weight = nn.Parameter(U @ torch.sqrt(sigma))
+                    self.lora_model.model.layers[groupby_index].sub_layers[layer_index].mlp.B_up.weight = nn.Parameter(torch.sqrt(sigma) @ V)
                     # W_down
                     down_diff = self.down_layer_dict["model.layers.{real_layer}".format(real_layer = real_layer)] - self.lora_model.model.layers[groupby_index].W_down.weight
                     U, sigma, V = torch.svd(down_diff)
                     U = U[:,:self.lora_size]
                     sigma = torch.diag(sigma[:self.lora_size])
                     V = V[:,:self.lora_size].T
-                    self.lora_model.model.layers[groupby_index].sub_layers[layer_index].mlp.A_down.weight.detach().copy_(U @ torch.sqrt(sigma))
-                    self.lora_model.model.layers[groupby_index].sub_layers[layer_index].mlp.B_down.weight.detach().copy_(torch.sqrt(sigma) @ V)
+                    self.lora_model.model.layers[groupby_index].sub_layers[layer_index].mlp.A_down.weight = nn.Parameter(U @ torch.sqrt(sigma))
+                    self.lora_model.model.layers[groupby_index].sub_layers[layer_index].mlp.B_down.weight = nn.Parameter(torch.sqrt(sigma) @ V)
     
     def init(self)->None:
         self.replace_state(self.config)
